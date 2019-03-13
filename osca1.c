97570ebaa499a7b0072d4d58f6e4ecde236492c5
Gytis1995 <norutisgytis@yahoo.com>
2019-03-13 15:23:44 +0000

    put random number in to function

--- osca1.c
+++ osca1.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-
-int i;
 int start = 2048;
 int end = 20480;
-int middle = end - start;
-int randomNumber;
-start = 2048;
-end = 20480;
-for(i=0;i<=20;i++){
-//int generatorRandomNumber(int start, int end){
-randomNumber = rand() % (middle+1) + start ;
-printf("random number between 2048 and 20480 %d\n",randomNumber);
-}
-return 0;
+
+int generateRandomNumber(int start, int end);
+
+int main(){
+
+	int r = generateRandomNumber(start, end);
+	printf("Random number generated is : %d\n", r); 
+
+	return 0;
+
 }
 
+int generateRandomNumber(int start, int end){
+	int mid = end- start;
+	int	randomNumber;
+
+	randomNumber = rand() % (mid+1) + start;
+
+	return randomNumber;	
+}
