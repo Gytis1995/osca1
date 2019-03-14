665fb94106b142ba5788e76a5882d33124f11aac
Gytis1995 <norutisgytis@yahoo.com>
2019-03-14 16:15:27 +0000

    generating random characters

--- osca1.c
+++ osca1.c
@@ -3,13 +3,25 @@
 
 int start = 2048;
 int end = 20480;
+int arraySize = 65536;
 
 int generateRandomNumber(int start, int end);
 
 int main(){
 
 	int r = generateRandomNumber(start, end);
-	printf("Random number generated is : %d\n", r); 
+	printf("Random number generated is : %d\n", r);
+	
+	char n[arraySize];
+	int i;
+	int r2;
+	for(i=0;i<=r;i++)
+	{
+int rand = 	generateRandomNumber(32,126);
+char n = rand;
+		printf("random char: %c\n",n);
+	}  
+
 
 	return 0;
 
@@ -23,3 +35,4 @@ int generateRandomNumber(int start, int end){
 
 	return randomNumber;	
 }
+
